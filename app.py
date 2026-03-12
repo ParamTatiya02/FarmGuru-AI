2e4fdd9c338045951f9850fd4e35fc3ec961bf68
ParamTatiya02 <paramtatiya@gmail.com>
2026-03-12 16:41:19 +0530

    Setup basic libraries and API key

---
+++ app.py
@@ -0,0 +1,11 @@
+from secret_key import sarvam_ai_key
+import os
+import streamlit as st
+from sarvamai import SarvamAI
+
+#Initialize the Sarvam AI secret API key
+os.environ['SARVAM_API_KEY'] = sarvam_ai_key
+client = SarvamAI(api_subscription_key=os.environ['SARVAM_API_KEY'])
+
+#Web page
+st.title("FarmGuru Chatbot")
